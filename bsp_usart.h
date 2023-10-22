5732091d230402c58d2437ced42557170e1e05c4
yaoyilin123 <130761502+yaoyilin123@users.noreply.github.com>
2023-10-22 21:56:40 +0800

    Update bsp_usart.h

--- bsp_usart.h
+++ bsp_usart.h
@@ -3,6 +3,13 @@
 
 #include "stm32f10x.h"
 
+#define USART_1  1
+#define USART_2  0
+#define USART_3  0
+#define USART_4  0
+#define USART_5  0
+
+#if  USART_1
 // 串口1-USART1
 #define  DEBUG_USARTx                   USART1
 #define  DEBUG_USART_CLK                RCC_APB2Periph_USART1
@@ -21,6 +28,87 @@
 #define  DEBUG_USART_IRQ                USART1_IRQn
 #define  DEBUG_USART_IRQHandler         USART1_IRQHandler
 
+#elif  USART_2
+// 串口2-USART2
+#define  DEBUG_USARTx                   USART2
+#define  DEBUG_USART_CLK                RCC_APB1Periph_USART2
+#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+#define  DEBUG_USART_BAUDRATE           115200
+
+// USART GPIO 引脚宏定义
+#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOA)
+#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+    
+#define  DEBUG_USART_TX_GPIO_PORT       GPIOA   
+#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_2
+#define  DEBUG_USART_RX_GPIO_PORT       GPIOA
+#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_3
+
+#define  DEBUG_USART_IRQ                USART2_IRQn
+#define  DEBUG_USART_IRQHandler         USART2_IRQHandler
+
+#elif  USART_3
+// 串口3-USART3
+#define  DEBUG_USARTx                   USART3
+#define  DEBUG_USART_CLK                RCC_APB1Periph_USART3
+#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+#define  DEBUG_USART_BAUDRATE           115200
+
+// USART GPIO 引脚宏定义
+#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOB)
+#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+    
+#define  DEBUG_USART_TX_GPIO_PORT       GPIOB   
+#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_10
+#define  DEBUG_USART_RX_GPIO_PORT       GPIOB
+#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_11
+
+#define  DEBUG_USART_IRQ                USART3_IRQn
+#define  DEBUG_USART_IRQHandler         USART3_IRQHandler
+
+#elif  USART_4
+// 串口4-UART4
+#define  DEBUG_USARTx                   UART4
+#define  DEBUG_USART_CLK                RCC_APB1Periph_UART4
+#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+#define  DEBUG_USART_BAUDRATE           115200
+
+// USART GPIO 引脚宏定义
+#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOC)
+#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+    
+#define  DEBUG_USART_TX_GPIO_PORT       GPIOC   
+#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_10
+#define  DEBUG_USART_RX_GPIO_PORT       GPIOC
+#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_11
+
+#define  DEBUG_USART_IRQ                UART4_IRQn
+#define  DEBUG_USART_IRQHandler         UART4_IRQHandler
+
+#elif  USART_5
+// 串口5-UART5
+#define  DEBUG_USARTx                   UART5
+#define  DEBUG_USART_CLK                RCC_APB1Periph_UART5
+#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+#define  DEBUG_USART_BAUDRATE           115200
+
+// USART GPIO 引脚宏定义
+#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD)
+#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+    
+#define  DEBUG_USART_TX_GPIO_PORT       GPIOC  
+#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_12
+#define  DEBUG_USART_RX_GPIO_PORT       GPIOD 
+#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_2
+
+#define  DEBUG_USART_IRQ                UART5_IRQn
+#define  DEBUG_USART_IRQHandler         UART5_IRQHandler
+
+#endif
 void USART_Config(void);
-void Usart_SendBit(USART_TypeDef* PUSARTx, uint16_t Data);
+void Usart_SendBit(USART_TypeDef* PUSARTx, uint8_t Data);
+void Usart_SendHalfWord(USART_TypeDef* PUSARTx, uint16_t Data);
+void Usart_SendArr(USART_TypeDef* PUSARTx, uint8_t *arr,uint8_t num);
+void Usart_SendStr(USART_TypeDef* PUSARTx,char *str);
+void showMessage(void);
 #endif   /*__BSP_USART_H*/
