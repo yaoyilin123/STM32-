b874395b5daa2e688c11c2bf0e695c63890db223
yaoyilin123 <130761502+yaoyilin123@users.noreply.github.com>
2023-11-06 16:57:33 +0800

    Update bsp_I2C.h

--- bsp_I2C.h
+++ bsp_I2C.h
@@ -10,4 +10,5 @@ void I2C_RandRead_Config(uint8_t add,uint8_t *data,uint8_t NumByte);
 void I2C_ByetWrite_Config(uint8_t add,uint8_t data);
 void detection_GenerateSta(void);
 void I2C_PageWrite(uint8_t addr,uint8_t *data,uint8_t NumByte);
+void I2C_EE_BufferWrite(uint8_t WriteAddr,uint8_t *Data,uint8_t NumWrite);
 #endif   /*__BSP_I2C_H*/
